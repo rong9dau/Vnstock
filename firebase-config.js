a1b2b9e62582ac056dccd3a95b10a915105f7db7
rong9dau <rong9dau@yahoo.com>
2026-05-05 10:38:49 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-// Dán Firebase config thật vào đây nếu muốn đồng bộ nhiều thiết bị.
-// Nếu không dùng Firebase, cứ để file này như hiện tại và bấm "Dùng offline".
+// Dán config Firebase Web App của bạn vào đây.
+// Khi apiKey/projectId còn PASTE_..., app chạy local và chưa đồng bộ đa thiết bị.
 export const firebaseConfig = {
-  apiKey: "DAN_API_KEY_VAO_DAY",
-  authDomain: "DAN_PROJECT_ID.firebaseapp.com",
-  projectId: "DAN_PROJECT_ID",
-  storageBucket: "DAN_PROJECT_ID.appspot.com",
-  messagingSenderId: "DAN_MESSAGING_SENDER_ID",
-  appId: "DAN_APP_ID"
+  apiKey: "AIzaSyCZEtwM8zaTY2rhgK8JZiJfla0CptfHb7Q",
+  authDomain: "gptv5-f27c9.firebaseapp.com",
+  projectId: "gptv5-f27c9",
+  storageBucket: "gptv5-f27c9.firebasestorage.app",
+  messagingSenderId: "471211541747",
+  appId: "1:471211541747:web:785149435bda517466e386",
+  measurementId: "G-K1NGPDVGTF"
 };
